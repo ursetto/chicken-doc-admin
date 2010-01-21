77811a4e2a038f750a165d3a875a710c5e442f53
Jim <jim@amaranth.xorinia.dim>
2010-01-21 00:07:28 -0600

    read-svnwiki: add some condition handling

--- tst/read-svnwiki.scm
+++ tst/read-svnwiki.scm
@@ -3,14 +3,25 @@
 (use chicken-syntax)
 (use extras) ; read-file
 
-(print
- (eval `(begin
-          (use eggdoc eggdoc-svnwiki)
-          (eggdoc:warnings #f)
-          (eggdoc:svnwiki-override!)
-          (with-output-to-string
-            (lambda ()
-              ,@(read-file file))))
-       (interaction-environment)))
+(let ((str
+       (condition-case
+        (eval `(begin
+                 (use eggdoc eggdoc-svnwiki)
+                 (eggdoc:warnings #f)
+                 (eggdoc:svnwiki-override!)
+                 (with-output-to-string
+                   (lambda ()
+                     ,@(read-file file)))
+                 ))
+        (e (exn)
+           (apply warning
+                  (string-append "Parse failure: "
+                                 (let ((loc ((condition-property-accessor 'exn 'location) e)))
+                                   (if loc (conc loc ": ") ""))
+                                 (or ((condition-property-accessor 'exn 'message) e) ""))
+                  ((condition-property-accessor 'exn 'arguments) e))
+           #f))))
+  (when str
+    (print str)))
 
 
